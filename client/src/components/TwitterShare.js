dff1ef6def7f81269194fdeea448ef55e9227f7c
James Barrington <james.barrington1985@gmail.com>
2020-06-29 21:10:52 +0100

    social_share_butt_on_all_pages

--- client/src/components/TwitterShare.js
+++ client/src/components/TwitterShare.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+  TwitterIcon
+ } from "react-share";
 
 import '../styles/sass/components/_twitter.scss';
 
@@ -7,7 +10,7 @@ const TwitterShare = ({ quote, author }) => {
     <>
       <div className="twitter__share">
         <a href={`https://twitter.com/intent/tweet?text= ${quote} - ${author} - philosofind.co.uk`} target="_blank" rel="noopener noreferrer" title="Post this quote on twitter!" id='tweet-quote'>
-          <i className="fab fa-twitter fa-3x" id='#twitterbtn' />
+          <TwitterIcon size={32} round={true}/>
         </a>
       </div>
     </>
